753be2fb6cfbb157aff19ad1c3a0bdb06f6fca15
Shay D. <dardenteysha@gmail.com>
2025-06-16 00:05:30 -0500

    Removed useEffect.

--- src/components/Pages/Trivia.js
+++ src/components/Pages/Trivia.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { motion } from "framer-motion";
 import { pageVariants, pageTransition } from "../../animations/pageTransitions";
 import { triviaQuestions } from "../../data/triviaData";
