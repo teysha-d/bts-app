ea6f926133677ba2727467cd6eef1190aa536d52
Shay D. <dardenteysha@gmail.com>
2025-06-16 13:01:41 -0500

    Added more photos to the carousel and updated my code.

--- src/components/Pages/Gallery.js
+++ src/components/Pages/Gallery.js
@@ -2,6 +2,9 @@
 import React, { useState } from "react";
 import Lightbox from "yet-another-react-lightbox";
 import "yet-another-react-lightbox/styles.css";
+
+// ⬇️ Manually import each image
+import groupPhoto from "../../images/bts1.jpg";
 import rm from "../../images/bts2.jpg";
 import jin from "../../images/bts3.jpg";
 import suga from "../../images/bts4.jpg";
@@ -9,7 +12,15 @@ import jhope from "../../images/bts5.jpg";
 import jimin from "../../images/bts6.jpg";
 import v from "../../images/bts7.jpg";
 import jungkook from "../../images/bts8.jpg";
-import groupPhoto from "../../images/bts1.jpg";
+
+import photo9 from "../../images/bts9.jpg";
+import photo10 from "../../images/bts10.jpg";
+import photo11 from "../../images/bts11.jpg";
+import photo12 from "../../images/bts12.jpg";
+import photo13 from "../../images/bts13.jpg";
+import photo14 from "../../images/bts14.jpg";
+import photo15 from "../../images/bts15.jpg";
+
 import { motion } from "framer-motion";
 import { pageVariants, pageTransition } from "../../animations/pageTransitions";
 
@@ -23,17 +34,22 @@ export default function Gallery() {
     { src: jimin, title: "Jimin" },
     { src: v, title: "V" },
     { src: jungkook, title: "Jungkook" },
+
+    // And now your new photos:
+    { src: photo9, title: "Extra 1" },
+    { src: photo10, title: "Extra 2" },
+    { src: photo11, title: "Extra 3" },
+    { src: photo12, title: "Extra 4" },
+    { src: photo13, title: "Extra 5" },
+    { src: photo14, title: "Extra 6" },
+    { src: photo15, title: "Extra 7" },
   ];
 
   const [current, setCurrent] = useState(0);
   const [open, setOpen] = useState(false);
 
-  const prev = () => {
-    setCurrent((i) => (i === 0 ? images.length - 1 : i - 1));
-  };
-  const next = () => {
-    setCurrent((i) => (i === images.length - 1 ? 0 : i + 1));
-  };
+  const prev = () => setCurrent((i) => (i === 0 ? images.length - 1 : i - 1));
+  const next = () => setCurrent((i) => (i === images.length - 1 ? 0 : i + 1));
   const openLightbox = () => setOpen(true);
 
   return (
@@ -47,7 +63,7 @@ export default function Gallery() {
     >
       <h2>BTS Photo Gallery 📸</h2>
 
-      {/* Centered Group Photo */}
+      {/* Group Photo */}
       <div className="gallery-group">
         <img
           src={groupPhoto}
@@ -64,12 +80,10 @@ export default function Gallery() {
         <button className="carousel-nav prev" onClick={prev}>
           ‹
         </button>
-
         <div className="carousel-slide" onClick={openLightbox}>
           <img src={images[current].src} alt={images[current].title} />
           <p className="slide-title">{images[current].title}</p>
         </div>
-
         <button className="carousel-nav next" onClick={next}>
           ›
         </button>
