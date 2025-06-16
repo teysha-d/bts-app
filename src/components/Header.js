63885d279be1c81bd3e2bc181c9d0513fc79d68d
Shay D. <dardenteysha@gmail.com>
2025-06-15 23:59:20 -0500

    Change made....

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import btsLogo from "../../assets/bts-logo.svg";
+import btsLogo from "../assets/bts-logo.svg";
 
 export default function Header() {
   return (
